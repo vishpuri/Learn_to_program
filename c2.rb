0753cfc6f4c1c7ad89989a4735a2322199c301c5
Vishal Puri <vishal_puri@hotmail.com>
2013-07-29 23:10:44 -0400

    changes

---
+++ c2.rb
@@ -0,0 +1,6 @@
+puts "hours in a year"
+puts 365*24
+puts 'minutes in a decade'
+puts 10*365*24*60
+puts 'age in seconds'
+puts (2013-1900)*365*24*60*60
---
+++ c5b.rb
@@ -0,0 +1,4 @@
+puts 'what is your favourite number?'
+favnum = gets.chomp
+newfavnum = favnum.to_i + 1
+puts newfavnum
